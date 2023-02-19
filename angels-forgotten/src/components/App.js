6f232e55fbbdc4fcea97ddebe3bae25e39aedd1e
Brian Wangila <bwanjalah@gmail.com>
2023-02-19 08:56:48 +0300

    adds component files

---
+++ angels-forgotten/src/components/App.js
@@ -0,0 +1,24 @@
+
+import './App.css';
+import LandingPage from './LandingPage';
+import Gallery from './Gallery'
+import AboutUs from './AboutUs';
+import Donate from './Donate';
+import NavBar from './NavBar'
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello</h1>
+      <LandingPage/>
+      <Gallery/>
+      <AboutUs/>
+      <Donate/>
+      <NavBar/>
+    </div>
+  );
+}
+
+export default App;
---
+++ angels-forgotten/src/components/NavBar.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+function NavBar(){
+  return(
+    <h1>hey</h1>
+  )
+}
+
+export default NavBar;
